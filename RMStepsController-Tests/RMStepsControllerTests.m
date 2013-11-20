a9a35478c5fe89c8dcaf0e0ec3941992a7d12a8c
CooperRS <CooperRS@yasit.net>
2013-11-20 19:32:22 +0100

    Added unit test target to fix a crash in Travis

---
+++ RMStepsController-Tests/RMStepsControllerTests.m
@@ -0,0 +1,20 @@
+//
+//  RMStepsControllerTests.m
+//  RMStepsController-Demo
+//
+//  Created by Roland Moers on 20.11.13.
+//  Copyright (c) 2013 Roland Moers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RMStepsControllerTests : XCTestCase
+
+@end
+
+@implementation RMStepsControllerTests
+
+- (void)testExample {
+}
+
+@end
